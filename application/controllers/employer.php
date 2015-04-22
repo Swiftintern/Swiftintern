2c1958d3a48a853672943c9dcd83aee52b528c9d
faizanayubi <indianayubi@gmail.com>
2015-04-22 19:49:40 +0530

    Employer

--- application/controllers/employer.php
+++ application/controllers/employer.php
@@ -5,10 +5,11 @@
  *
  * @author Faizan Ayubi
  */
+use Shared\Controller as Controller;
 use Framework\Registry as Registry;
 use Framework\RequestMethods as RequestMethods;
 
-class Employer extends Users {
+class Employer extends Controller {
 
     /**
      * @readwrite
