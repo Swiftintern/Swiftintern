149c505b848c752b037da21f24173612b9715e3b
faizanayubi <indianayubi@gmail.com>
2015-05-08 21:03:56 +0530

    Secured CRON

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -5,24 +5,25 @@
  *
  * @author Faizan Ayubi
  */
-
 use Shared\Controller as Controller;
 use Framework\Registry as Registry;
 use Framework\RequestMethods as RequestMethods;
 
-class CRON extends Controller{
-    
+class CRON extends Controller {
+
     public function __construct($options = array()) {
         parent::__construct($options);
         $this->willRenderLayoutView = false;
         $this->willRenderActionView = false;
     }
-    
+
+    /**
+     * @before _secure
+     */
     public function index() {
-        //echo php_sapi_name();
         $this->notifications();
     }
-    
+
     protected function newsletters() {
         $now = strftime("%Y-%m-%d", strtotime('now'));
         $newsletters = Newsletter::all(array("scheduled = ?" => $now));
@@ -30,15 +31,25 @@ class CRON extends Controller{
             $users = User::first();
         }
     }
-    
+
     protected function notifications() {
         $yesterday = strftime("%Y-%m-%d", strtotime('-1 day'));
-        $applications = Application::all(array("updated = ?" => $yesterday), array("id","student_id","opportunity_id","status"));
+        $applications = Application::all(array("updated = ?" => $yesterday), array("id", "student_id", "opportunity_id", "status"));
         echo count($applications);
     }
-    
+
     public function trackImage($property, $property_id) {
         
     }
-    
+
+    /**
+     * @protected
+     */
+    public function _secure() {
+        //echo php_sapi_name();
+        if ($_SERVER['REMOTE_ADDR'] != $_SERVER['SERVER_ADDR']) {
+            die('access is not permitted');
+        }
+    }
+
 }
