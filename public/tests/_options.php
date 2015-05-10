0dce132ccd851fb7a82c1bb3559f37ed95fc9e65
faizanayubi <indianayubi@gmail.com>
2015-05-10 11:22:14 +0530

    Tests Added

---
+++ public/tests/_options.php
@@ -0,0 +1,11 @@
+<?php
+
+$options = array(
+    "type" => "mysql",
+    "options" => array(
+        "host" => "localhost",
+        "username" => "swiftintern",
+        "password" => "pass",
+        "schema" => "swiftintern"
+    )
+);
