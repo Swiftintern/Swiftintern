a818a6c9c1324d3d51e64c21e5a68be60e865026
faizanayubi <indianayubi@gmail.com>
2015-05-17 11:05:17 +0530

    Added Mail Controller

---
+++ application/controllers/mail.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Contains all mail to be sent by various controllers
+ *
+ * @author Faizan Ayubi
+ */
+use Shared\Controller as Controller;
+use Framework\Registry as Registry;
+use Framework\RequestMethods as RequestMethods;
+
+class Mail extends Users{
+    public function index() {
+        
+    }
+}
