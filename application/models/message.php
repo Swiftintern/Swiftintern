25be36c6674ca1a89eefa0fb20ae066a0a9af8c5
faizanayubi <indianayubi@gmail.com>
2015-05-17 14:51:41 +0530

    Updated Message Model

--- application/models/message.php
+++ application/models/message.php
@@ -10,20 +10,6 @@ class Message extends Shared\Model {
     /**
      * @column
      * @readwrite
-     * @type integer
-     */
-    protected $_from_user_id;
-    
-    /**
-     * @column
-     * @readwrite
-     * @type integer
-     */
-    protected $_to_user_id;
-
-    /**
-     * @column
-     * @readwrite
      * @type text
      * @length 256
      */
@@ -34,38 +20,5 @@ class Message extends Shared\Model {
      * @readwrite
      * @type text
      */
-    protected $_message;
-    
-    /**
-     * @column
-     * @readwrite
-     * @type integer
-     */
-    protected $_validity;
-
-    /**
-     * fetches a list of replies to a message. It also returns the user_name of the user who replied, 
-     * and sorts the replies by their creation date, so that newer messages are displayed first.
-     * @return type
-     */
-    public function getReplies() {
-        return self::all(array(
-            "message = ?" => $this->getId(),
-            "live = ?" => true,
-            "deleted = ?" => false
-        ), array(
-            "*",
-            "(SELECT CONCAT(first, \" \", last) FROM user WHERE user.id = message.user)" => "user_name"
-        ),
-        "created",
-        "desc");
-    }
-
-    public static function fetchReplies($id) {
-        $message = new Message(array(
-            "id" => $id
-        ));
-        return $message->getReplies();
-    }
-
+    protected $_body;
 }
