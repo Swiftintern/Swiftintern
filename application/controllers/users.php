322d19f2de5944194be95245231e59ef31992e39
faizanayubi <indianayubi@gmail.com>
2015-05-21 17:42:04 +0530

    Merged Mail Controller with Users

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -6,14 +6,43 @@
  * @author Faizan Ayubi
  */
 use Shared\Controller as Controller;
-use Framework\Registry as Registry;
-use Framework\RequestMethods as RequestMethods;
 
 class Users extends Controller {
+    
+    protected $_sendgrid;
+    private static $_template = array(
+        "STUDENT_REGISTER" => "1",
+        "INTERNSHIP_VERIFIED" => "2",
+        "APPLICATION_SELECTED" => "3",
+        "APPLICATION_REJECTED" => "4",
+        "APPLICATION_INTERNSHIP" => 5
+    );
+    
+    protected function mail() {
+        $configuration = Registry::get("configuration");
+        $configuration = $configuration->initialize();
+        $parsed = $configuration->parse("configuration/sendgrid");
 
+        if (!empty($parsed->sendgrid->default) && !empty($parsed->sendgrid->default->username)) {
+            $sendgrid = new \SendGrid\SendGrid($parsed->sendgrid->default->username, $parsed->sendgrid->default->password);
+            $this->_sendgrid = $sendgrid;
+        }
+    }
+    
     /**
-     * Destroys session of current user
+     * @before mail
      */
+    protected function notify($user, $type) {
+        $mail = Message::first(array("id = ?"=> self::$_template[$type]));
+        $email = new \SendGrid\Email();
+        $email->addTo($user->email)
+            ->setFrom('info@swiftintern.com')
+            ->setFromName('Saud Akhtar')
+            ->setSubject($mail->subject)
+            ->setHtml($mail->body);
+        $this->_sendgrid->send($email);
+    }
+
     public function logout() {
         $this->setUser(false);
         self::redirect("/home");
