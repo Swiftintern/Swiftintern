dcadb45533fdf27d3cd6ea512889ca00f94eaa0a
faizanayubi <indianayubi@gmail.com>
2015-05-25 21:01:13 +0530

    Started Resume for Internship Module

--- application/controllers/resumes.php
+++ application/controllers/resumes.php
@@ -19,22 +19,31 @@ class Resumes extends Students {
             "view" => $this->getLayoutView()
         ));$view = $this->getActionView();
         
-        $li = $this->LinkedIn("http://swiftintern.com/students/register");
+        $li = $this->LinkedIn("http://swiftintern.com/resumes");
         if (isset($_REQUEST['code'])) {
             $li->getAccessToken($_REQUEST['code']);
-            self::redirect('/success');
+            //self::redirect('/resumes/success');
         } else {
-            $url = $li->getLoginUrl(array(LinkedIn::SCOPE_FULL_PROFILE, LinkedIn::SCOPE_EMAIL_ADDRESS, LinkedIn::SCOPE_CONTACT_INFO));
+            $url = $li->getLoginUrl(array(LinkedIn::SCOPE_BASIC_PROFILE, LinkedIn::SCOPE_EMAIL_ADDRESS));
             $view->set("url", $url);
         }
+        $this->getActionView()->set("li", $li);
+    }
+    
+    public function guidelines() {
+        $this->seo(array(
+            "title" => "Internship Resume Guideline",
+            "keywords" => "guidelines to create internship resume",
+            "description" => "Swiftintern.com is a great place to build and post your resume online for free. Its easy to sign up, free to use, and you can access your resume from anywhere once you have posted it. Use our free resume builder to create the perfect resume online in minutes.",
+            "view" => $this->getLayoutView()
+        ));
     }
 
     /**
      * @before _secure
      */
     public function success() {
-        $this->noview();
-        $this->seo(array(
+        $this->seo(array( 
             "title" => "Resume",
             "keywords" => "resume",
             "description" => "Edit resume online",
@@ -45,8 +54,8 @@ class Resumes extends Students {
         if ($li->hasAccessToken()) {
             $info = $li->get('/people/~:(phone-numbers,summary,first-name,last-name,positions,email-address,public-profile-url,location,picture-url,educations,skills)');
         }
-        
-        echo '<pre>', print_r($info), '</pre>';
+        $view->set('info', $info);
+        //echo '<pre>', print_r($info), '</pre>';
     }
 
 }
