30c066aa750e13f42fd23c020e5135c21c9c8e3e
faizanayubi <indianayubi@gmail.com>
2015-05-28 12:13:40 +0530

    Updated OnlineTest Module
    Changed name of methods and respective html views

--- application/controllers/onlinetest.php
+++ application/controllers/onlinetest.php
@@ -54,12 +54,38 @@ class OnlineTest extends Users {
 
         $view->set("test", $test);
     }
+    
+    /**
+     * @before _secure
+     */
+    public function test($title, $id) {
+        $test = Test::first(array("id = ?" => $id), array("id", "title", "syllabus", "type", "subject", "time_limit"));
+        $this->seo(array(
+            "title" => $test->title,
+            "keywords" => $title,
+            "description" => strip_tags($test->syllabus),
+            "view" => $this->getLayoutView()
+        ));
+        $view = $this->getActionView();
 
-    public function result() {
-        $this->noview();
-        echo '<pre>', print_r($_POST), '</pre>';
-        
-        /*$participant = Participant::first(array("id = ?" => $participant_id));
+        $questions = Question::all(array("test_id = ?" => $test->id), array("id", "question", "type"));
+
+        $participant = Participant::first(array("test_id = ?" => $test->id, "user_id = ?" => $this->user->id));
+        if(!$participant){
+            $participant = new Participant(array(
+                "test_id" => $test->id,
+                "user_id" => $this->user->id,
+                "score" => "", "time_taken" => "", "attempted" => ""
+            ));$participant->save();
+        }
+
+        $view->set("participant", $participant);
+        $view->set("test", $test);
+        $view->set("questions", $questions);
+    }
+
+    public function result($participant_id) {
+        $participant = Participant::first(array("id = ?" => $participant_id));
         $test = Test::first(array("id = ?" => $participant->test_id));
         $this->seo(array(
             "title" => $test->title,
@@ -69,23 +95,23 @@ class OnlineTest extends Users {
         ));$view = $this->getActionView();
 
         if (RequestMethods::post("action") == "test_result") {
-            $questions = Question::all(array("test_id = ?" => $test->id), array("id", "question", "type"));
-            $per_ques = 100 / count($questions);$marks = 0;$count = 0;
+            $total_questions = Question::count(array("test_id = ?" => $test->id));
+            $per_ques = 100 /$total_questions;$marks = 0;$count = 0;
+            $questions = RequestMethods::post("question");
             
-            foreach ($_POST as $field_name => $val) {
-                $field_name = strip_tags(trim($field_name));
-                if (strpos($field_name, 'question') !== false) {
-                    $name = explode('_', $field_name);
-                    $ques_id = $name[1];
-                    $option_id = filterdata($val);
-                    $option = Option::find_by_id('id', $option_id);
-                    if ($option->is_answer) {
-                        $marks += $per_ques;
-                    }
-                    $count++;
-                }
+            foreach ($questions as $question => $answer) {
+                $option = Option::first(array("id = ?" => $answer), array("is_answer"));
+                if ($option->is_answer) {
+                    $marks += $per_ques;
+                }$count++;
             }
-        }*/
+            $participant->score = $marks;
+            $participant->attempted = $count;
+            
+            $participant->save();
+        }
+        $view->set("participant", $participant);
+        $view->set("test", $test);
     }
 
     public function test_certi($certi_id) {
@@ -119,31 +145,6 @@ class OnlineTest extends Users {
         $view->set("certificate", $certificate);
     }
 
-    /**
-     * @before _secure
-     */
-    public function test($title, $id) {
-        $test = Test::first(array("id = ?" => $id), array("id", "title", "syllabus", "type", "subject", "time_limit"));
-        $this->seo(array(
-            "title" => $test->title,
-            "keywords" => $title,
-            "description" => strip_tags($test->syllabus),
-            "view" => $this->getLayoutView()
-        ));
-        $view = $this->getActionView();
-
-        $questions = Question::all(array("test_id = ?" => $test->id), array("id", "question", "type"));
-
-        $participant = Participant::first(array("test_id = ?" => $test->id, "user_id = ?" => $this->user->id));
-
-        $view->set("participant", $participant);
-        $view->set("test", $test);
-        $view->set("questions", $questions);
-    }
-
-    /**
-     * @param type $test
-     */
     public function test_participated($title, $id) {
         $seo = Framework\Registry::get("seo");
         $view = $this->getActionView();
@@ -166,33 +167,6 @@ class OnlineTest extends Users {
         $this->getLayoutView()->set("seo", $seo);
         $view->set("test", $test);
         $view->set("photo", $photo);
-    }
-
-    /**
-     * @before _secure
-     * @param type $participant_id
-     */
-    public function test_result($participant_id) {
-        //fetching participant details
-        $participant = Participant::first(array('id = ?' => $participant_id));
-        $user = User::first(array('id = ?' => $participant->user_id));
-        $student = Student::first(array('user_id = ?' => $participant->user_id));
-
-        //fetching test details
-        $test = Test::first(array('id = ?' => $participant->test_id));
-
-        $seo = Framework\Registry::get("seo");
-        $view = $this->getActionView();
-
-        $seo->setTitle("Result of {$test->title} by {$user->name}");
-        $seo->setKeywords("test result");
-        $seo->setDescription("Result of {$test->title} by {$user->name}");
-        $this->getLayoutView()->set("seo", $seo);
-
-        $view->set("user", $user);
-        $view->set("participant", $participant);
-        $view->set("student", $student);
-        $view->set("test", $test);
     }
 
     public function certification() {
