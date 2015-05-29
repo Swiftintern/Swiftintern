2ee9f6b5c1ac8f919dcdacce5153b0fbd70adaac
faizanayubi <indianayubi@gmail.com>
2015-05-29 20:47:26 +0530

    Added Logging Method to Stats Model

--- application/models/stat.php
+++ application/models/stat.php
@@ -6,6 +6,7 @@
  * @author Faizan Ayubi
  */
 class Stat extends Shared\Model {
+
     /**
      * @column
      * @readwrite
@@ -13,26 +14,39 @@ class Stat extends Shared\Model {
      * @length 32
      */
     protected $_property;
-    
+
     /**
      * @column
      * @readwrite
      * @type integer
      */
     protected $_property_id;
-    
-    /**
-     * @column
-     * @readwrite
-     * @type datetime
-     */
-    protected $_date;
-    
-    
+
     /**
      * @column
      * @readwrite
      * @type integer
      */
     protected $_viewed;
+
+    public static function log($property, $property_id) {
+        $date = strftime("%Y-%m-%d", strtotime('now'));
+        $stat = Stat::first(array(
+            "property = ?" => $property,
+            "property_id = ?" => $property_id,
+            "created LIKE ?" => $date
+        ));
+        if($stat){
+            $stat->viewed += 1;
+        } else {
+            $stat = new Stat(array(
+                "property" => $property,
+                "property_id" => $property_id,
+                "viewed" => "1"
+            ));
+        }
+        $stat->save();
+        return true;
+    }
+
 }
