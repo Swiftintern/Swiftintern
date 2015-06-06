74df4197cce4539fdf35a81d2d746c7db3fa7eac
faizanayubi <indianayubi@gmail.com>
2015-06-06 21:39:10 +0530

    Added CRM Lead Model

---
+++ application/models/lead.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Description of crm
+ *
+ * @author Faizan Ayubi
+ */
+class Lead extends Shared\Model {
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 256
+     */
+    protected $_email;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_crm_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 32
+     */
+    protected $_status;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_validity;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type datetime
+     */
+    protected $_updated;
+}
