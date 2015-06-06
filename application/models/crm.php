1658c7bd85e807b671563a08286b1afec55a5d7f
faizanayubi <indianayubi@gmail.com>
2015-06-06 22:03:17 +0530

    Created CRM Template Model

---
+++ application/models/crm.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Description of crm
+ *
+ * @author Faizan Ayubi
+ */
+class CRM extends Shared\Model {
+
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_user_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 128
+     */
+    protected $_title;
+
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_first_message_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_second_message_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_third_message_id;
+
+}
