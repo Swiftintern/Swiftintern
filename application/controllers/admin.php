52f08ac457c00b2f78b88974dd02b28479e3448d
faizanayubi <indianayubi@gmail.com>
2015-06-07 13:59:41 +0530

    Completed CRM Lead Generation

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -32,27 +32,28 @@ class Admin extends Users {
             $key = RequestMethods::get("key");
             $value = RequestMethods::get("value");
             $r = new ReflectionClass(ucfirst($model));
-            
+
             $object = $r->newInstanceWithoutConstructor()->all(array("{$key} = ?" => $value));
             $results = $object;
             //echo '<pre>', print_r($results), '</pre>';
             $view->set("results", $results);
         }
     }
-    
-    public function update($model=NULL, $id=NULL) {
+
+    public function update($model = NULL, $id = NULL) {
         $this->changeLayout();
         $this->seo(array("title" => "Update", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
-        
+
         $object = $model::first(array("id = ?" => $id));
-        
-        $vars = $object->columns;$array = array();
+
+        $vars = $object->columns;
+        $array = array();
         foreach ($vars as $key => $value) {
             array_push($array, $key);
             $vars[$key] = htmlentities($object->$key);
         }
-        if(RequestMethods::post("action")=="update"){
+        if (RequestMethods::post("action") == "update") {
             foreach ($array as $field) {
                 $object->$field = RequestMethods::post($field, $vars[$field]);
                 $vars[$field] = htmlentities($object->$field);
@@ -60,23 +61,24 @@ class Admin extends Users {
             $object->save();
             $view->set("success", true);
         }
-        
+
         $view->set("vars", $vars);
         $view->set("array", $array);
         $view->set("model", $model);
         $view->set("id", $id);
     }
-    
+
     public function crmTemplate() {
         $this->changeLayout();
         $this->seo(array("title" => "CRM", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
-        if(RequestMethods::post("action") == "createCrmTemplate"){
+        if (RequestMethods::post("action") == "createCrmTemplate") {
             $body = RequestMethods::post("message");
             $subject = RequestMethods::post("subject");
             foreach ($body as $key => $value) {
-                $msg = new Message(array("subject" => $subject[$key],"body" => $value));
-                $msg->save();$message[] = $msg;
+                $msg = new Message(array("subject" => $subject[$key], "body" => $value));
+                $msg->save();
+                $message[] = $msg;
             }
             $crm = new CRM(array(
                 "user_id" => $this->user->id,
@@ -88,25 +90,53 @@ class Admin extends Users {
             $view->set("success", TRUE);
         }
     }
-    
+
     public function crmLead() {
         $this->changeLayout();
         $this->seo(array("title" => "Lead Generation", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
-        
-        $crms = CRM::all(array(), array("id","title"));
+
+        if (RequestMethods::post("action") == "leadGeneration") {
+            if (RequestMethods::post("emails")) {
+                $emails = explode(",", RequestMethods::post("emails"));
+            } else {
+                $emails = array();
+            }
+            if (!empty($_FILES['file']['name'])) {
+                $tmpName = $_FILES['file']['tmp_name'];
+                $csvAsArray = array_map('str_getcsv', file($tmpName));
+                foreach ($csvAsArray as $key => $value) {
+                    array_push($emails, $value[0]);
+                }
+            }
+
+            foreach ($emails as $email) {
+                $lead = new Lead(array(
+                    "user_id" => $this->user->id,
+                    "email" => $email,
+                    "crm_id" => RequestMethods::post("crm_id"),
+                    "status" => "started",
+                    "validity" => "1",
+                    "updated" => ""
+                ));
+                $lead->save();
+            }
+            $view->set("success", TRUE);
+        }
+
+        $crms = CRM::all(array(), array("id", "title"));
         $view->set("crms", $crms);
     }
-    
+
     public function crmManage() {
         $this->changeLayout();
         $this->seo(array("title" => "Manage CRM", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
-        
+
         $page = RequestMethods::get("page", 1);
         $limit = RequestMethods::get("limit", 10);
         $leads = Lead::all(array("user_id = ?" => $this->user->id), array("*"), "created", "desc", $limit, $page);
-        
+
         $view->set("limit", $limit);
         $view->set("page", $page);
         $view->set("leads", $leads);
