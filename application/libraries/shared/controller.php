05e35dcffeb36e4f72ac60db2bffd5c40c98b99f
faizanayubi <indianayubi@gmail.com>
2015-06-08 21:55:56 +0530

    Updated Secure Method

--- application/libraries/shared/controller.php
+++ application/libraries/shared/controller.php
@@ -38,7 +38,7 @@ namespace Shared {
         public function _secure() {
             $user = $this->getUser();
             if (!$user) {
-                header("Location: /students/register");
+                header("Location: /home");
                 exit();
             }
         }
