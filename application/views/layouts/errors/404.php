327d6baf413a41df3d591601ad68c795df661199
faizanayubi <indianayubi@gmail.com>
2015-06-14 19:37:57 +0530

    Designed 404 Page

--- application/views/layouts/errors/404.php
+++ application/views/layouts/errors/404.php
@@ -1,12 +1,147 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html">
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="The page you are looking for is not found" />
+        <meta name="keywords" content="error not found">
+        <meta name="copyright" content="SWIFTDEAL ONLINE LLP 2015">
+        <meta name="email" content="info[at]swiftintern[dot]com">
+        <meta http-equiv="Content-Language" content="en">
+        <meta name="Rating" content="General">
+        <meta name="Distribution" content="Global">
+        <meta name="Revisit-after" content="1 Day">
+
         <title>Error 404</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link href="http://assets.swiftintern.com/css/custom-1.0.css" rel="stylesheet" />
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+
     </head>
     <body>
-        error 404
+        <nav class="navbar" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Be sure to leave the brand out there if you want it shown -->
+                    <a class="navbar-brand" href="/"><i class="fa fa-graduation-cap fa-lg"></i> SwiftIntern</a>
+                </div>
+
+                <!-- post or upload cv -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href=""></a></li>
+                        <li><a href="/resume-for-internship" class="whitetext"><i class="fa fa-file-text-o fa-lg"></i> Create Resume</a></li>
+                        <li><a href="/onlinetest" class="whitetext hidden-sm"><i class="fa fa-clock-o fa-lg"></i> Certification Test</a></li>
+                        <li><a href="/placement-papers" class="whitetext"><i class="fa fa-files-o fa-lg"></i> Placement Papers</a></li>
+                    </ul>
+                    <!-- the menu -->
+                    <div class="navbar-right">
+                        <ul class="nav navbar-nav">
+
+                            <li><a href="/students/register" class="whitetext"><i class="fa fa-user-plus fa-lg"></i> Students</a></li>
+                            <li><a href="/employer/register" class="whitetext"><i class="fa fa-briefcase fa-lg"></i> Employer</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+        <header class="sb-page-header">
+            <div class="container">
+                <h1>Error 404</h1>
+                <p>The page you are looking for is not found.</p>
+            </div>
+        </header>
+        
+        <section class="container">
+            <article class="row text-center">
+                Go back to,  <a href="/" class="btn btn-primary text-center">home page</a>
+            </article>
+        </section>
+        
         <?php if (DEBUG): ?>
             <pre><?php print_r($e); ?></pre>
         <?php endif; ?>
+
+        <footer class="hidden-print">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <h4><i class="fa fa-graduation-cap fa-lg"></i>SwiftIntern</h4>
+                        <nav>
+                            <ul class="list-unstyled list-social-icons">
+                                <li><a href="/about" class="whitetext">About</a></li>
+                                <li><a href="/blog" class="whitetext">Intern Blog</a></li>
+                                <li><a href="/termsofservice" class="whitetext">Terms of Use</a></li>
+                                <li><a href="/privacy" class="whitetext">Privacy</a></li>
+                                <li><a href="/contact" class="whitetext">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <h4><i class="fa fa-user fa-lg"></i> Students</h4>
+                        <nav>
+                            <ul class="list-unstyled list-social-icons">
+                                <li><a href="/student/register" class="whitetext">Register</a></li>
+                                <li><a href="/resume-for-internship" class="whitetext">Resume for Internship</a></li>
+                                <li><a href="/onlinetest" class="whitetext">Online Test</a></li>
+                                <li><a href="/placement-papers" class="whitetext">See Placement Papers</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <h4><i class="fa fa-briefcase fa-lg"></i> Employers</h4>
+                        <nav>
+                            <ul class="list-unstyled list-social-icons">
+                                <li><a href="/employer/register" class="whitetext">Register your Company</a></li>
+                                <li><a href="/employer/about" class="whitetext">About/Benefits</a></li>
+                                <li><a href="/employer/postinternship" class="whitetext">Post Internship</a></li>
+                                <li><a href="/organizations" class="whitetext">Companies</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <h4><i class="fa fa-users fa-lg"></i> Follow Us</h4>
+                        <nav>
+                            <ul class="list-unstyled list-social-icons">
+                                <li><i class="fa fa-facebook"></i><a href="https://facebook.com/swiftintern" class="whitetext" target="_blank"> Facebook</a></li>
+                                <li><i class="fa fa-twitter"></i><a href="https://twitter.com/swiftintern" class="whitetext" target="_blank"> Twitter</a></li>
+                                <li><i class="fa fa-google-plus"></i><a href="https://plus.google.com/+Swiftinternindia" rel="publisher" class="whitetext" target="_blank"> Google+</a></li>
+                                <li><i class="fa fa-linkedin"></i><a href="https://www.linkedin.com/company/swiftintern-com" class="whitetext" target="_blank"> LinkedIn</a></li>
+                                <a href="https://plus.google.com/+Swiftinternindia" rel="publisher">Google+</a>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <hr>
+                <p>
+                    <!--<button class="whitetext btn btn-success btn-xs" name="message" value=""><i class="fa fa-comments-o fa-fw"></i> Got a Question?</button>-->
+                    <span class="pull-right">
+                        <a href="http://www.swiftdeal.org/" class="whitetext"><i class="fa fa-copyright fa-fw"></i> SwiftDeal Online LLP.</a>
+                    </span>
+                </p>
+                <p class="text-center">
+                    <i class="fa fa-credit-card fa-lg"></i> <i class="fa fa-cc-visa fa-lg"></i> <i class="fa fa-cc-mastercard fa-lg"></i> <i class="fa fa-cc-discover fa-lg"></i>
+                </p>
+            </div>
+        </footer>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $(".navbar").css("margin-bottom", "0px");
+            });
+        </script>
     </body>
 </html>
