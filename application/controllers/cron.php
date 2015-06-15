cd2de0f155d43b4b2641f3e352e844f91e70eb4b
faizanayubi <indianayubi@gmail.com>
2015-06-15 08:51:22 +0530

    Added Employer Notifications CRON

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -16,14 +16,19 @@ class CRON extends Users {
 
     public function index() {
         $this->_secure();
+        $this->leads();
+        $this->log("Leads Sent");
         $this->newsletters();
         $this->log("Newsletters Sent");
-        $this->leads();
-        $this->log("Leasd Sent");
         $this->notifications();
         $this->log("Notification Sent");
+        $this->newApplications();
+        $this->log("newApplications Sent");
     }
 
+    /**
+     * Follow Leads, Send Further Message for completing Campaign
+     */
     protected function leads() {
         $date = strftime("%Y-%m-%d", strtotime('-4 days'));
         $now = strftime("%Y-%m-%d", strtotime('now'));
@@ -32,12 +37,12 @@ class CRON extends Users {
         foreach ($leads as $lead) {
             $crm = CRM::first(array("id = ?" => $lead->crm_id), array("second_message_id"));
             $message = Message::first(array("id = ?" => $crm->second_message_id));
-            $lds = Lead::all(array("created = ?" => $date, "crm_id = ?" => $lead->crm_id), array("email", "user_id"));
+            $lds = Lead::all(array("created = ?" => $date, "crm_id = ?" => $lead->crm_id));
             foreach ($lds as $ld) {
                 $exist = User::first(array("email = ?" => $ld->email), array("id"));
                 $user = User::first(array("id = ?" => $ld->user_id), array("id","name","email"));
                 if (!$exist) {
-                    $lead->status = "SECOND_MESSAGE_SENT";
+                    $ld->status = "SECOND_MESSAGE_SENT";
                     $this->notify(array(
                         "template" => "leadGeneration",
                         "subject" => $message->subject,
@@ -47,31 +52,30 @@ class CRON extends Users {
                         "emails" => array($ld->email)
                     ));
                 } else {
-                    $lead->status = "REGISTERED";
+                    $ld->status = "REGISTERED";
                 }
                 
-                $lead->updated = $now;
-                $lead->save();
+                $ld->updated = $now;
+                $ld->save();
             }
         }
     }
 
+    /**
+     * Sends Newsletters to User Group
+     */
     protected function newsletters() {
         $now = strftime("%Y-%m-%d", strtotime('now'));
-        $emails = array();
-        $limit = 0;
-        $count = 0;
+        $emails = array();$limit = 0;$count = 0;
 
         $newsletters = Newsletter::all(array("scheduled = ?" => $now));
         foreach ($newsletters as $newsletter) {
             $message = Message::first(array("id = ?" => $newsletter->message_id));
             $users = User::all(array("type = ?" => $newsletter->user_group), array("email"));
             foreach ($users as $user) {
-                $emails[$limit][] = $user->email;
-                $count++;
+                $emails[$limit][] = $user->email;$count++;
                 if ($count == '999') {
-                    $count = 0;
-                    $limit++;
+                    $count = 0;$limit++;
                 }
             }
 
@@ -88,6 +92,9 @@ class CRON extends Users {
         }
     }
 
+    /**
+     * Send Notifications to students for their application status
+     */
     protected function notifications() {
         $yesterday = strftime("%Y-%m-%d", strtotime('-1 day'));
         $applications = Application::all(array("updated LIKE ?" => "%{$yesterday}%"), array("id", "student_id", "opportunity_id", "status"));
@@ -114,6 +121,30 @@ class CRON extends Users {
                     ));
                     break;
             }
+        }
+    }
+    
+    /**
+     * Send Notifications to employers about number of students applied
+     */
+    protected function newApplications() {
+        $created = strftime("%Y-%m-%d", strtotime('-1 day'));$emails = array();
+        $applications = Application::all(array("created LIKE ?" => "%{$created}%"), array("DISTINCT opportunity_id"));
+        foreach ($applications as $application) {
+            $opportunity = Opportunity::first(array("id = ?" => $application->opportunity_id), array("title", "id", "organization_id"));
+            $applicants = Application::count(array("created LIKE ?" => "%{$created}%", "opportunity_id = ?" => $application->opportunity_id));
+            $members = Member::all(array("organization_id = ?" => $opportunity->organization_id),array("user_id"));
+            foreach ($members as $member) {
+                $mem = User::first(array("user_id = ?" => $member->user_id),array("email"));
+                array_push($emails, $mem->email);
+            }
+            $this->notify(array(
+                "template" => "employerApplicants",
+                "opportunity" => $opportunity,
+                "applicants" => $applicants,
+                "emails" => $emails
+            ));
+            $this->log(implode("", $emails));
         }
     }
 
