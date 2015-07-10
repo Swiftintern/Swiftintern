c965fa6338c752541b4cf34fdec6df89883fee1d
faizanayubi <indianayubi@gmail.com>
2015-07-10 18:18:04 +0530

    Made Link Track method

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -33,6 +33,58 @@ class Users extends Controller {
     }
 
     /**
+     * Redirects to external link from html views with analytics
+     * @param string $url the url to redirect to
+     */
+    public function link($url, $num) {
+        $this->noview();
+        static $http = array(
+            100 => "HTTP/1.1 100 Continue",
+            101 => "HTTP/1.1 101 Switching Protocols",
+            200 => "HTTP/1.1 200 OK",
+            201 => "HTTP/1.1 201 Created",
+            202 => "HTTP/1.1 202 Accepted",
+            203 => "HTTP/1.1 203 Non-Authoritative Information",
+            204 => "HTTP/1.1 204 No Content",
+            205 => "HTTP/1.1 205 Reset Content",
+            206 => "HTTP/1.1 206 Partial Content",
+            300 => "HTTP/1.1 300 Multiple Choices",
+            301 => "HTTP/1.1 301 Moved Permanently",
+            302 => "HTTP/1.1 302 Found",
+            303 => "HTTP/1.1 303 See Other",
+            304 => "HTTP/1.1 304 Not Modified",
+            305 => "HTTP/1.1 305 Use Proxy",
+            307 => "HTTP/1.1 307 Temporary Redirect",
+            400 => "HTTP/1.1 400 Bad Request",
+            401 => "HTTP/1.1 401 Unauthorized",
+            402 => "HTTP/1.1 402 Payment Required",
+            403 => "HTTP/1.1 403 Forbidden",
+            404 => "HTTP/1.1 404 Not Found",
+            405 => "HTTP/1.1 405 Method Not Allowed",
+            406 => "HTTP/1.1 406 Not Acceptable",
+            407 => "HTTP/1.1 407 Proxy Authentication Required",
+            408 => "HTTP/1.1 408 Request Time-out",
+            409 => "HTTP/1.1 409 Conflict",
+            410 => "HTTP/1.1 410 Gone",
+            411 => "HTTP/1.1 411 Length Required",
+            412 => "HTTP/1.1 412 Precondition Failed",
+            413 => "HTTP/1.1 413 Request Entity Too Large",
+            414 => "HTTP/1.1 414 Request-URI Too Large",
+            415 => "HTTP/1.1 415 Unsupported Media Type",
+            416 => "HTTP/1.1 416 Requested range not satisfiable",
+            417 => "HTTP/1.1 417 Expectation Failed",
+            500 => "HTTP/1.1 500 Internal Server Error",
+            501 => "HTTP/1.1 501 Not Implemented",
+            502 => "HTTP/1.1 502 Bad Gateway",
+            503 => "HTTP/1.1 503 Service Unavailable",
+            504 => "HTTP/1.1 504 Gateway Time-out"
+        );
+        header($http[$num]);
+        header("Location: " . base64_decode($url));
+        exit();
+    }
+
+    /**
      * The Main Method to return SendGrid Instance
      * 
      * @return \SendGrid\SendGrid Instance of Sendgrid
@@ -77,7 +129,7 @@ class Users extends Controller {
         $this->noview();
         echo '<h1>Unsubscribed Successfully</h1>';
     }
-    
+
     protected function recipientVariables($emails) {
         $json = array();
         foreach ($emails as $email) {
@@ -85,7 +137,7 @@ class Users extends Controller {
                 "cat" => "newsletter"
             );
         }
-        
+
         return json_encode($json, JSON_PRETTY_PRINT);
     }
 
@@ -131,10 +183,10 @@ class Users extends Controller {
         }
         $this->log(implode(",", $emails));
     }
-    
+
     protected function trackUser($user) {
         $now = strftime("%Y-%m-%d %H:%M:%S", strtotime('now'));
-        
+
         $user->last_login = $now;
         $user->login_number = $this->user->login_number + 1;
         $user->last_ip = $_SERVER['REMOTE_ADDR'];
@@ -158,7 +210,7 @@ class Users extends Controller {
         $this->willRenderLayoutView = false;
         $this->willRenderActionView = false;
     }
-    
+
     public function JSONview() {
         $this->willRenderLayoutView = false;
         $this->defaultExtension = "json";
@@ -207,7 +259,7 @@ class Users extends Controller {
      * @param string $name
      * @param string $type files or images
      */
-    protected function _upload($name, $type="files") {
+    protected function _upload($name, $type = "files") {
         if (isset($_FILES[$name])) {
             $file = $_FILES[$name];
             $path = APP_PATH . "/public/assets/uploads/{$type}/";
