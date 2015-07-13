abc05a242a4147d1d31b16d920eb990780cde276
faizanayubi <indianayubi@gmail.com>
2015-07-14 01:29:38 +0530

    Started Quick Apply Method

--- application/controllers/opportunities.php
+++ application/controllers/opportunities.php
@@ -8,8 +8,8 @@
 use Framework\Registry as Registry;
 use Framework\RequestMethods as RequestMethods;
 
-class Opportunities extends Users{
-    
+class Opportunities extends Students {
+
     public function index($title, $id) {
         $this->JSONview();
         $view = $this->getActionView();
@@ -26,13 +26,13 @@ class Opportunities extends Users{
 
         $view->set("opportunity", $opportunity);
     }
-    
+
     public function internship($title, $id) {
         global $datetime;
         $view = $this->getActionView();
         $session = Registry::get("session");
         $student = $session->get("student");
-        
+
         $opportunity = Opportunity::first(array("id = ?" => $id));
         $organization = Organization::first(array("id = ?" => $opportunity->organization_id), array("id", "name", "photo_id"));
         if ($student) {
@@ -74,11 +74,75 @@ class Opportunities extends Users{
         $view->set("opportunity", $opportunity);
         $view->set("organization", $organization);
     }
-    
+
+    public function quickApply() {
+        $user = $this->read(array(
+            "model" => "user",
+            "where" => array("email = ?" => RequestMethods::post("email"))
+        ));
+        if ($user) {
+            $social = $this->read(array(
+                "model" => "social",
+                "where" => array("user_id = ?" => $user->id, "social_platform = ?" => "linkedin")
+            ));
+            $student = Student::first(array("user_id = ?" => $user->id));
+            $this->trackUser($user);
+        } else {
+            $user = new User(array(
+                "name" => $info["firstName"] . " " . $info["lastName"],
+                "email" => $info["emailAddress"],
+                "phone" => $this->checkData($info["phoneNumbers"]["values"][0]["phoneNumber"]),
+                "password" => rand(100000, 99999999),
+                "access_token" => rand(100000, 99999999),
+                "type" => "student",
+                "validity" => "1",
+                "last_ip" => $_SERVER['REMOTE_ADDR'],
+                "last_login" => "1",
+                "updated" => ""
+            ));
+            $user->save();
+            $this->notify(array(
+                "template" => "studentRegister",
+                "subject" => "Getting Started on Swiftintern.com",
+                "user" => $user
+            ));
+
+            //add student
+            $skills = "";
+            if ($info["skills"]["_total"] > 0) {
+                foreach ($info["skills"]["values"] as $key => $value) {
+                    $skills .= $value["skill"]["name"];
+                    $skills .= ",";
+                }
+            }
+            $student = new Student(array(
+                "user_id" => $user->id,
+                "about" => $this->checkData($info["summary"]),
+                "city" => $this->checkData($info["location"]["name"]),
+                "skills" => $skills,
+                "updated" => ""
+            ));
+            $student->save();
+        }
+
+        if (!$social) {
+            $social = new Social(array(
+                "user_id" => $user->id,
+                "social_platform" => "linkedin",
+                "link" => $this->checkData($info["publicProfileUrl"])
+            ));
+            $social->save();
+            $this->linkedinDetails($info, $student);
+        }
+
+        $info["user"] = $user;
+        $this->login($info, $student);
+    }
+
     public function competition($title, $id) {
         global $datetime;
         $view = $this->getActionView();
-        
+
         $opportunity = Opportunity::first(array("id = ?" => $id));
         $organization = Organization::first(array("id = ?" => $opportunity->organization_id), array("id", "name", "photo_id"));
 
@@ -94,7 +158,7 @@ class Opportunities extends Users{
         $view->set("opportunity", $opportunity);
         $view->set("organization", $organization);
     }
-    
+
     public function sponsored() {
         $this->JSONview();
         global $datetime;
@@ -119,7 +183,5 @@ class Opportunities extends Users{
         }
         $this->getActionView()->set("sponsoreds", $sponsoreds);
     }
-    
-    
-    
+
 }
