732273257821a3513cb26b7d89d626cec44b1e11
faizanayubi <indianayubi@gmail.com>
2015-08-10 20:24:58 +0530

    Added GTM

--- public/assets/js/employer-3.0.js
+++ public/assets/js/employer-3.0.js
@@ -3,6 +3,13 @@
     window.opts = {};
 }(window, window.Model));
 
+//Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KFFXD4');
+
 $(function () {
     $('#side-menu').metisMenu();
 });
@@ -36,23 +43,6 @@ $(function () {
         }
     })
 });
-
-//Google Analytics
-(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-49272936-3', 'swiftintern.com');
-ga('require', 'linkid', 'linkid.js');
-ga('send', 'pageview');
 
 $(document).ready(function () {
 
