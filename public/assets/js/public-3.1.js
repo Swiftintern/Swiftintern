8d6b338aff430eb0a867caba933c3f3f38d540ed
Faizan Ayubi <indianayubi@gmail.com>
2015-10-14 02:08:11 +0530

    Removed OWL js

--- public/assets/js/public-3.1.js
+++ public/assets/js/public-3.1.js
@@ -10,34 +10,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KFFXD4');
 
-$(document).ready(function() {
- 
-  var owl = $("#owl-demo");
- 
-  owl.owlCarousel({
-      items : 10, //10 items above 1000px browser width
-      itemsDesktop : [1000,6], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,5], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
-      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-  });
- 
-  // Custom Navigation Events
-  $(".next").click(function(){
-    owl.trigger('owl.next');
-  })
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
-  })
-  $(".play").click(function(){
-    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  })
-  $(".stop").click(function(){
-    owl.trigger('owl.stop');
-  })
- 
-});
-
 $(document).ready(function () {
 
     var dt = new Date(),
