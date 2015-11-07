2af8f851aaa88a9aab47487bd03d7eb60753f92b
Faizan Ayubi <indianayubi@gmail.com>
2015-11-07 19:23:40 +0530

    Sidebar Navigation Collapse Improved

--- public/assets/js/employer-3.0.js
+++ public/assets/js/employer-3.0.js
@@ -20,28 +20,37 @@ $(function () {
     });
 });
 
-//Loads the correct sidebar on window load
-$(function () {
-    $(window).bind("load", function () {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
-});
 
-//Collapses the sidebar on window resize
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
 $(function () {
-    $(window).bind("resize", function () {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
+    $(window).bind("load resize", function () {
+        topOffset = 50;
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
         } else {
-            $('div.sidebar-collapse').removeClass('collapse')
+            $('div.navbar-collapse').removeClass('collapse');
         }
-    })
+
+        height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1)
+            height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    var element = $('ul.nav a').filter(function () {
+        return this.href == url || url.href.indexOf(this.href) == 0;
+    }).addClass('active').parent().parent().addClass('in').parent();
+    if (element.is('li')) {
+        element.addClass('active');
+    }
 });
 
 $(document).ready(function () {
