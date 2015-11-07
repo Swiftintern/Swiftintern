5a9990564bc7ac9293b00a8e8a9801663b6c7507
Faizan Ayubi <indianayubi@gmail.com>
2015-11-07 19:12:39 +0530

    added zopim chat

--- public/assets/js/employer-3.0.js
+++ public/assets/js/employer-3.0.js
@@ -101,3 +101,10 @@ $(document).ready(function () {
         });
     });
 });
+
+//Start of Zopim Live Chat Script
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3RxEIkzsUZsvxcaF9qoCaxQcPVvDYeJJ";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
